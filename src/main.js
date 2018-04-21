6e5f855d85efae9750224042a2bf9b0bbe99889a
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-04-21 20:34:04 +0100

    Init

---
+++ src/main.js
@@ -0,0 +1,38 @@
+/*
+Ludum Dare 41
+-------------
+
+Game jam project for Ludum Dare 41, built on the theme of Combine 2 Incompatible
+Genres.
+
+(Shaun A. Noordin | shaunanoordin.com | 20180421)
+
+--------------------------------------------------------------------------------
+ */
+
+/*  Primary App Class
+ */
+//==============================================================================
+class App {
+  constructor() {
+    this.html = {
+      app: document.getElementById("app"),
+      canvas: document.getElementById("canvas"),
+    };
+    
+    
+  
+    
+    
+  }
+}
+//==============================================================================
+
+/*  Initialisations
+ */
+//==============================================================================
+var app;
+window.onload = function() {
+  window.app = new App();
+};
+//==============================================================================
