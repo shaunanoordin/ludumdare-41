e7dad266c9247248cdd567a518af8c1741c259a7
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-04-21 20:48:48 +0100

    Get basic code from AvO

--- src/main.js
+++ src/main.js
@@ -10,21 +10,190 @@ Genres.
 --------------------------------------------------------------------------------
  */
 
+import * as APP from "./constants.js";  //Naming note: all caps.
+import { Utility } from "./utility.js";
+
 /*  Primary App Class
  */
 //==============================================================================
 class App {
   constructor() {
+    //Initialise properties
+    //--------------------------------
+    this.config = {
+      framesPerSecond: APP.FRAMES_PER_SECOND,
+      debugMode: false,
+      autoFitCanvas: true,
+    };    
     this.html = {
       app: document.getElementById("app"),
       canvas: document.getElementById("canvas"),
     };
+    this.context2d = this.html.canvas.getContext("2d");
+    this.boundingBox = null;  //To be defined by this.updateSize().
+    this.canvasSizeRatio = 1;
+    this.canvasWidth = this.html.canvas.width;  //The intended width/height of the canvas.
+    this.canvasHeight = this.html.canvas.height;
+    this.state = null;
+    //--------------------------------
     
+    //Account for graphical settings
+    //--------------------------------
+    this.context2d.mozImageSmoothingEnabled = false;
+    this.context2d.msImageSmoothingEnabled = false;
+    this.context2d.imageSmoothingEnabled = false;
+    //--------------------------------
     
-  
+    //Initialise Game Objects
+    //--------------------------------
+    this.assets = {
+      images: {}
+    };
+    this.assetsLoaded = 0;
+    this.assetsTotal = 0;
+    //--------------------------------
     
+    //Prepare Input
+    //--------------------------------
+    this.keys = new Array(APP.MAX_KEYS);
+    for (let i = 0; i < this.keys.length; i++) {
+      this.keys[i] = {
+        state: APP.INPUT_IDLE,
+        duration: 0
+      };
+    }
+    this.pointer = {
+      start: { x: 0, y: 0 },
+      now: { x: 0, y: 0 },
+      state: APP.INPUT_IDLE,
+      duration: 0
+    };
+    //--------------------------------
     
+    //Bind Events
+    //--------------------------------
+    if ("onmousedown" in this.html.canvas && "onmousemove" in this.html.canvas &&
+        "onmouseup" in this.html.canvas) {
+      this.html.canvas.onmousedown = this.onPointerStart.bind(this);
+      this.html.canvas.onmousemove = this.onPointerMove.bind(this);
+      this.html.canvas.onmouseup = this.onPointerEnd.bind(this);
+    }    
+    if ("ontouchstart" in this.html.canvas && "ontouchmove" in this.html.canvas &&
+        "ontouchend" in this.html.canvas && "ontouchcancel" in this.html.canvas) {
+      this.html.canvas.ontouchstart = this.onPointerStart.bind(this);
+      this.html.canvas.ontouchmove = this.onPointerMove.bind(this);
+      this.html.canvas.ontouchend = this.onPointerEnd.bind(this);
+      this.html.canvas.ontouchcancel = this.onPointerEnd.bind(this);
+    }
+    if ("onkeydown" in window && "onkeyup" in window) {
+      window.onkeydown = this.onKeyDown.bind(this);
+      window.onkeyup = this.onKeyUp.bind(this);
+    }
+    if ("onresize" in window) {
+      window.onresize = this.updateSize.bind(this);
+    }
+    this.updateSize();
+    //--------------------------------
+    
+    //Start!
+    //--------------------------------
+    this.runCycle = setInterval(this.run.bind(this), 1000 / this.config.framesPerSecond);
+    //--------------------------------
   }
+  
+  //----------------------------------------------------------------
+  
+  run() {
+    this.paint();
+  }
+  
+  //----------------------------------------------------------------
+  
+  paint() {
+    //Clear
+    this.context2d.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+  }
+  
+  //----------------------------------------------------------------
+  
+  onPointerStart(e) {
+    this.pointer.state = APP.INPUT_ACTIVE;
+    this.pointer.duration = 1;
+    this.pointer.start = this.getPointerXY(e);
+    this.pointer.now = this.pointer.start;
+    return Utility.stopEvent(e);
+  }
+  
+  onPointerMove(e) {
+    if (this.pointer.state === APP.INPUT_ACTIVE) {
+      this.pointer.now = this.getPointerXY(e);
+    }
+    return Utility.stopEvent(e);
+  }
+  
+  onPointerEnd(e) {
+    this.pointer.state = APP.INPUT_ENDED;
+    //this.pointer.now = this.getPointerXY(e);
+    return Utility.stopEvent(e);
+  }
+  
+  getPointerXY(e) {
+    let clientX = 0;
+    let clientY = 0;
+    if (e.clientX && e.clientY) {
+      clientX = e.clientX;
+      clientY = e.clientY;
+    } else if (e.touches && e.touches.length > 0 && e.touches[0].clientX &&
+        e.touches[0].clientY) {
+      clientX = e.touches[0].clientX;
+      clientY = e.touches[0].clientY;
+    }
+    let inputX = (clientX - this.boundingBox.left) * this.canvasSizeRatio;
+    let inputY = (clientY - this.boundingBox.top) * this.canvasSizeRatio;
+    return { x: inputX, y: inputY };
+  }
+  
+  //----------------------------------------------------------------
+  
+  onKeyDown(e) {
+    let keyCode = Utility.getKeyCode(e);
+    if (keyCode > 0 && keyCode < APP.MAX_KEYS && this.keys[keyCode].state != APP.INPUT_ACTIVE) {
+      this.keys[keyCode].state = APP.INPUT_ACTIVE;
+      this.keys[keyCode].duration = 1;
+    }  //if keyCode == 0, there's an error.
+  }
+  
+  onKeyUp(e) {
+    let keyCode = Utility.getKeyCode(e);    
+    if (keyCode > 0 && keyCode < APP.MAX_KEYS) {
+      this.keys[keyCode].state = APP.INPUT_ENDED;
+    }  //if keyCode == 0, there's an error.
+  }
+  
+  //----------------------------------------------------------------
+  
+  updateSize() {
+    if (this.config.autoFitCanvas) {
+      const bestFit = Math.min(
+        this.html.app.offsetWidth / this.canvasWidth,
+        this.html.app.offsetHeight / this.canvasHeight
+      );
+      
+      this.html.canvas.style =
+        "width: " + Math.round(bestFit * this.canvasWidth) + "px; " +
+        "height: " + Math.round(bestFit * this.canvasHeight) + "px; ";
+    }
+    
+    let boundingBox = (this.html.canvas.getBoundingClientRect)
+      ? this.html.canvas.getBoundingClientRect()
+      : { left: 0, top: 0 };
+    this.boundingBox = boundingBox;
+    this.canvasSizeRatio = Math.min(
+      this.canvasWidth / this.boundingBox.width,
+      this.canvasHeight / this.boundingBox.height
+    );
+  }
+  
 }
 //==============================================================================
 
